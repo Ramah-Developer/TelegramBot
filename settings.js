0cf7416c6bae312b9ae0aa781d8aead41c072e3d
𝐑𝐀𝐌𝐀𝐇 𝐃𝐄𝐕 <andrewocholla9@gmail.com>
2024-12-08 04:41:34 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,21 +7,21 @@
 const fs = require("fs");
 
 global.BOT_TOKEN = "7437332589:AAGQCXC41R3QYRdRXvOG1naRQ2svz6xJ84s" // create a bot here https://t.me/Botfather and get bot tokens
-global.BOT_NAME = "FiiBotz" //your bot name
-global.OWNER_NAME = "AlfiXD" //your name
-global.OWNER_NUMBER = "62895615063060" //your telegram number
-global.OWNER = ["https://t.me/RafaMods", "https://t.me/alfisyahrial"] // create a bot here https://t.me/Botfather and get bot tokens
+global.BOT_NAME = "VERTEX-MD" //your bot name
+global.OWNER_NAME = "RMH" //your name
+global.OWNER_NUMBER = "256703025571" //your telegram number
+global.OWNER = ["https://t.me/Ramadhanyusuf", "https://t.me/Ramadhan0o"] // create a bot here https://t.me/Botfather and get bot tokens
 const {
    indonesia
 } = require("./lib");
 global.language = indonesia
 global.lang = language
-global.adminId = "7437332589"
-global.pp = 'https://files.catbox.moe/tttkp9.jpg',
+global.adminId = "7086635955"
+global.pp = 'https://pomf2.lain.la/f/viy5s1fl.jpg',
     //SERVER 1
-  global.domain = '', // Fill in the domain used
-  global.plta = '', // Fill in the appropriate hydropower value
-  global.pltc = '', // Fill in the appropriate pltc value
+  global.domain = 'https://ramahxhaki.nobodysey.me', // Fill in the domain used
+  global.plta = 'ptla_dOuHxRDNT6rAqs4vi0yMolDtASCWwtxViYYAl9frSWN', // Fill in the appropriate hydropower value
+  global.pltc = 'ptlc_emsuYdixlqkTcpJ0JOouZV7gpDhZ3KaZ1q9fkWigcq2', // Fill in the appropriate pltc value
   
   //CREATE PANEL
   global.loc = '1', // Fill in the desired location
